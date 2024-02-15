c3d5cac50748700031805762c48b6be0bff10c6a
Ryan Pham <rtpham10@gmail.com>
2024-02-15 00:09:43 -0800

    Completed reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = string;
+    let result = "";
+    reverse = reverse.split("").reverse();
+    result = reverse.join("");
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
