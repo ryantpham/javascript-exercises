be9145cbee471bf26511f6c825f51208078391e8
Ryan Pham <rtpham10@gmail.com>
2024-02-18 21:21:54 -0800

    Completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function(arr, ...num) {
-    return arr.filter(item => item != num);
+const removeFromArray = function(arr, ...numRemove) {
+    let newArr = [];
+    for (i=0; i<arr.length; i++){
+        if(!numRemove.includes(arr[i])){
+            newArr.push(arr[i])
+        }
+        
+    }
+    return newArr
 };
 
+console.log(removeFromArray([1,2,3,4],3)); //expect [1,2,4]
+console.log(removeFromArray([1,2,3,4],3,4)); //expect [1,2]
 // Do not edit below this line
 module.exports = removeFromArray;
