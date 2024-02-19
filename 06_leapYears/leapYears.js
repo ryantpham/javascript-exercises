826b62a74c8c80968c992a019ee30841d7c01d2d
Ryan Pham <rtpham10@gmail.com>
2024-02-18 22:31:12 -0800

    Completed leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof year != 'number'){
+        return "ERROR"
+    }
+
+    if (year % 400 == 0){
+        return true;
+    } else if (year % 100 ==0 && year & 400 == 0){
+        return false;
+    } else if (year % 4 ==0 && year % 100 != 0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
+//tests
+console.log(leapYears(2000)) // is a leap year: returns true
+console.log(leapYears(1985)) // is not a leap year: returns false
 // Do not edit below this line
 module.exports = leapYears;
