ed8d899b7a4ecb8e478d1fe35a0627cdf82014cb
Ryan Pham <rtpham10@gmail.com>
2024-02-18 22:55:01 -0800

    Completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  celsius = (fahrenheit - 32) * (5 / 9) * 10;
+  return Math.round(celsius) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  fahrenheit = ((celsius * 9) / 5 + 32) * 10;
+  return Math.round(fahrenheit) / 10;
 };
 
+//tests
+console.log(convertToCelsius(32)) // fahrenheit to celsius, should return 0
+console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
