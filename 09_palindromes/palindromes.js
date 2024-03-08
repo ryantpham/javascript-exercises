845e28b27dfbaba9dcd3585242a33976fd5bcf3b
Ryan Pham <rtpham10@gmail.com>
2024-03-07 22:25:12 -0800

    Completed first task of 09 palindromes where function only works for single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //Grabs String Backwards
+    let backwardArr = [];
+    for(let i = str.length -1; i >= 0; i--){
+        backwardArr.push(str[i]);
+    }
+    let backward = backwardArr.join('');
 
+    if(str === backward){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//tests
+console.log(palindromes('racecar')) // true
+console.log(palindromes('tacos')) // false
 
 // Do not edit below this line
 module.exports = palindromes;
+
+//pseudocode
+    //create empty array
+    //loop through string forward
+    //append to string to merge
+    //loop through string backwards
+    //append to string to merge
+    //if both are = to eachother return true else false
