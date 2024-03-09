5d42fa00653e6845aba2141b86e53698c0b8b3e3
Ryan Pham <rtpham10@gmail.com>
2024-03-08 21:28:27 -0800

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    } else if (num === 1){
+        return 1;
+    } else {
+        emptyArr = [0 , 1];
+        for (let i = 0; i < num; i++){
+            let c = emptyArr[i] + emptyArr[i + 1];
+            emptyArr.push(c);
+        }
+        return emptyArr[num];
+    }
+    
 };
-
+console.log(fibonacci(4)); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+console.log(fibonacci(6)); // returns 8
 // Do not edit below this line
 module.exports = fibonacci;
