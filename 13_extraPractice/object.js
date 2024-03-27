7d337e376e148bbcb2ed9ad48550e3d0850986e8
Ryan Pham <rtpham10@gmail.com>
2024-03-27 12:24:16 -0700

    Ran test code for factory function

--- 13_extraPractice/object.js
+++ 13_extraPractice/object.js
@@ -14,9 +14,13 @@ function HouseKeeper(yearsOfExperience,name,cleaningAreas){
     this.yearsOfExperience = yearsOfExperience;
     this.name = name;
     this.cleaningAreas = cleaningAreas;
+    this.clean = function clean(){
+        console.log('cleaning in progress')
+    }
 }
 
 //Test Code
 const houseKeeper2 = new HouseKeeper(4,'Jake',['bathroom','living room']);
 console.log(houseKeeper2);
 console.log(houseKeeper2.name);
+console.log(houseKeeper2.clean);
