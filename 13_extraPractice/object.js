d8b5682d947ffdb9a11ed254ddf599a7df30ba16
Ryan Pham <rtpham10@gmail.com>
2024-03-26 20:53:47 -0700

    practice set

---
+++ 13_extraPractice/object.js
@@ -0,0 +1,22 @@
+//Object
+const houseKeeper1 = {
+    yearsOfExperience: 2,
+    name: 'jane',
+    cleaningAreas:['bedroom','lobby','bathroom']
+}
+//Test Code
+console.log(houseKeeper1.name);
+console.log(houseKeeper1.yearsOfExperience);
+console.log(houseKeeper1.cleaningAreas);
+
+//Factory Function
+function HouseKeeper(yearsOfExperience,name,cleaningAreas){
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningAreas = cleaningAreas;
+}
+
+//Test Code
+const houseKeeper2 = new HouseKeeper(4,'Jake',['bathroom','living room']);
+console.log(houseKeeper2);
+console.log(houseKeeper2.name);
